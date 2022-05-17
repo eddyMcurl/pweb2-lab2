c2fc7ea7bfc88cd23446a56e667bafe35a89ec17
eddym <eddy@curl.cl>
2022-05-16 19:39:20 -0500

    logre quitar el guion

---
+++ ej4app.js
@@ -0,0 +1,12 @@
+function codigoLink(){
+    let url= document.getElementById('link').value;
+    let cadena =url;
+    let aux
+    let longitud = cadena.length;
+    let posSlash= url.lastIndexOf("/");
+    let cadenaCguion= url.slice(posSlash+1, longitud);
+    let cadenaSguion=cadenaCguion.replace("-","");
+    
+    
+    console.log(cadenaSguion);
+}
